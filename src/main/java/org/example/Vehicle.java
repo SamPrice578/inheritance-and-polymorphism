ee0567ace17bf0197890e85c7096e7373a391c18
Sam Price <samprice@Sams-MacBook-Pro.local>
2024-10-22 11:00:29 +0100

    feat: add engine attribute and constructor to Vehicle superclass

--- src/main/java/org/example/Vehicle.java
+++ src/main/java/org/example/Vehicle.java
@@ -4,4 +4,10 @@ public abstract class Vehicle {
     String make;
     String model;
     String speed;
+    Engine engine;
+
+    public Vehicle(String make, String model) {
+        this.make = make;
+        this.model = model;
+    }
 }
